771a2c7141b9c47cd819e0b28dee6f84a30acd53
Dzmitry Glebik <banan1314@gmail.com>
2021-10-29 21:53:06 +0300

    first commit

---
+++ script5.js
@@ -0,0 +1,9 @@
+/*5. Найдите площадь круга (SKruga) с радиусом 5см (r).*/
+
+console.log('Task number 5');
+
+let r = 5;
+
+SKruga = 3.14  * r * r;
+
+console.log(SKruga + 'см.кв')
---
+++ script8.js
@@ -0,0 +1,22 @@
+/*8. Решите уравнения (найдите неизвестный x), где a = 8, b = 3:
+a+2(x-b)=16;
+b(x+15)=a+6x;
+x+2x+ax+bx=23780.*/
+
+console.log('Task number 8');
+
+
+{let a = 8,
+     b = 3;
+
+x1 = ( 16 - a ) / 2 + b;
+
+console.log(x1);
+
+х2  = ( a - 15 * b ) / ( b - 6 );
+
+console.log(x2);
+
+х3  =  23780 / ( 1 + 2 + a + b );
+
+console.log(x3)}
